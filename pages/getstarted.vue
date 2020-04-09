f7205aa522308494c45220da31ad13a676c05b72
Sean Aye <hello@seanaye.ca>
2020-04-09 19:06:07 -0400

    add composition api and get started page

---
+++ pages/getstarted.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col>
+        <v-row justify="center">
+          <v-btn>New Wallet</v-btn>
+        </v-row>
+      </v-col>
+      <v-col>
+        <v-row justify="center">
+          <v-btn>Recover Wallet</v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup () {
+    
+  }
+})
+</script>
